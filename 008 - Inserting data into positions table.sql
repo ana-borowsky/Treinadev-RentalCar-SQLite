df4a3812f04a5a67467bb3de54aa8dcf44806594
Ana Paula Borowsky de Borba <anavonborowsky@gmail.com>
2022-12-27 21:40:44 -0300

    create and insert data into positions table

---
+++ 008 - Inserting data into positions table.sql
@@ -0,0 +1,22 @@
+-- Inserting data into positions table
+
+INSERT INTO POSITIONS (DESCRIPTION)
+    VALUES ('Gerente de vendas'),
+           ('Gerente de compras'),
+           ('Vendedor'),
+           ('Mecânico'),
+           ('Assistente Administrativo');
+
+
+
+
+
+
+
+
+
+
+
+
+
+

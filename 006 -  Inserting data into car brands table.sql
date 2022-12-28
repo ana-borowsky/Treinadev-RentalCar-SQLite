def78595843cc38ff2939637963fa851882d3daa
Ana Paula Borowsky de Borba <anavonborowsky@gmail.com>
2022-12-27 21:28:41 -0300

    create and insert data into car brand table

---
+++ 006 -  Inserting data into car brands table.sql
@@ -0,0 +1,23 @@
+-- Inserting data into car brands table
+
+INSERT INTO CAR_BRAND (BRAND_NAME)
+    VALUES ('Chevrolet'),
+           ('Toyota'),
+           ('Hyundai'),
+           ('Volkswagen'),
+           ('Jeep'),
+           ('Renault'),
+           ('Honda'),
+           ('Fiat');
+
+SELECT * FROM CAR_BRAND;
+
+
+
+
+
+
+
+
+
+

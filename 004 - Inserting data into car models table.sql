e5455ff8eac4ab71e9a18d7bc48fc0515f39366d
Ana Paula Borowsky de Borba <anavonborowsky@gmail.com>
2022-12-27 21:18:56 -0300

    creating and inserting data into car models table

---
+++ 004 - Inserting data into car models table.sql
@@ -0,0 +1,26 @@
+-- Inserting data into car models table
+INSERT INTO CAR_MODELS (MODEL_NAME)
+    VALUES ('Conversível'),
+           ('Sedã'),
+           ('Hatch'),
+           ('Coupé'),
+           ('Perua'),
+           ('SUV'),
+           ('Picape'),
+           ('Minivan'),
+           ('Utilitário'),
+           ('Buggy');
+
+SELECT * FROM CAR_MODELS
+
+
+
+
+
+
+
+
+
+
+
+
